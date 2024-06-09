b35e9200ac8520360e20aff622c849fc4170dbe7
sheehanms7 <144213636+sheehanms7@users.noreply.github.com>
2024-06-10 01:56:07 +0400

    Create Typography.js

---
+++ Typography.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+var word = "Hello world!";
+
+
+
+ function setup() { 
+
+
+
+createCanvas(600, 400); 
+
+background(255); fill(0); textSize(48); textAlign(CENTER);
+
+ text(word, width/2, height/2 - 30); text(textWidth(word), width/2, height/2 + 30); 
+
+}
